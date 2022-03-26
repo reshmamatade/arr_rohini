9f2d81ff09e23c1a994f5f8ae8a09ecbf9c2283d
shubham golhar <sgolhar79@gmail.com>
2022-03-26 17:01:20 +0530

    footer new

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -3,70 +3,90 @@ import "./footer.scss";
 
 export default function Footer() {
   return (
-    <footer className="page-footer font-small blue pt-4">
-      <div className="container-fluid text-center text-md-left">
+    <div className="container">
+      <div className="mt-5 pt-5 pb-5 footer">
         <div className="row">
-          <div className="col-md-6 mt-md-0 mt-3">
-            <h5 className="text-uppercase">About Us</h5>
-            <p>
+          <div className="col-lg-5 col-xs-12 aboutCompany">
+            <h2>About Us</h2>
+            <br />
+            <p className="pr-5 text-black-50 ">
               The A.R.R. Group was founded in the year 1947, by late Sri.
               A.R.Ramaswamy, Ex. Member of Legislative Assembly, Tamil Nadu and
               Ex.Chairman of Kumbakonam Municipality.
             </p>
-            <div>
-              <img src="https://img.icons8.com/small/16/000000/twitter.png" />
-              <img src="https://img.icons8.com/material-outlined/24/000000/facebook-new.png" />
-              <img src="https://img.icons8.com/ios/50/000000/instagram-new--v1.png" />
-            </div>
+            <p className="medialogo">
+              <a href="#">
+                <img src="https://img.icons8.com/ios-glyphs/30/000000/twitter--v1.png" />
+              </a>
+              <a href="#">
+                <img src="https://img.icons8.com/ios-glyphs/30/000000/instagram-new.png" />
+              </a>
+              <a href="#">
+                <img src="https://img.icons8.com/ios-glyphs/30/000000/facebook-new.png" />
+              </a>
+            </p>
           </div>
-
-          <hr className="clearfix w-100 d-md-none pb-0" />
-
-          <div className="col-md-3 mb-md-0 mb-3">
-            <h5 className="text-uppercase">Links</h5>
-            <ul className="list-unstyled ">
-              <li className="home">
-                <a href="#!">Home</a>
-              </li>
-              <li className="home">
-                <a href="#!">About Us</a>
-              </li>
-              <li className="home">
-                <a href="#!">Products</a>
+          <div className="col-lg-3 col-xs-12 links">
+            <h4 className="mt-lg-0 mt-sm-3">Useful Links</h4>
+            <br />
+            <ul className="m-0 p-0">
+              <li>
+                <a href="#">Home</a>
               </li>
               <li>
-                <a href="#!">Contact</a>
+                <a href="#">About Us</a>
+              </li>
+              <li>
+                <a href="#">Products</a>
+              </li>
+              <li>
+                <a href="#">Contact</a>
               </li>
             </ul>
           </div>
+          <div className="col-lg-4 col-xs-12 location">
+            <h4 className="mt-lg-0 mt-sm-4">Contact Us</h4>
+            <br />
 
-          <div className="col-md-3 mb-md-0 mb-3">
-            <h5 className="text-uppercase">Contact us</h5>
-            <ul className="list-unstyled">
-              <li>
-                <img />
-                No.153, A.R.R. Road, Kumbakonam, Thanjavur District, Tamil Nadu
-                - 612001
-              </li>
-              <li>
-                <img />
-                +(91) -88709 09988
-              </li>
-              <li>
-                <img />
-                contact@arrrohinifoodproducts.com
-              </li>
-            </ul>
+            <p>
+              <span>
+                <i className="mb-0" className="address">
+                  <img src="https://img.icons8.com/pastel-glyph/30/26e07f/marker.png" />
+                </i>
+              </span>
+              No.153, A.R.R. Road, Kumbakonam, Thanjavur District, Tamil Nadu -
+              612001
+            </p>
+            <p className="mb-0">
+              <span>
+                <i className="fa fa-phone mr-3" className="phone">
+                  <img src="https://img.icons8.com/ios/30/26e07f/phone.png" />
+                </i>
+              </span>
+              (+91)-8870909988
+            </p>
+            <p>
+              <span>
+                <i className="fa fa-envelope-o mr-3" className="message">
+                  <img src="https://img.icons8.com/external-flatart-icons-solid-flatarticons/30/26e07f/external-message-contact-flatart-icons-solid-flatarticons.png" />
+                </i>
+              </span>
+              contact@arrrohinifoodproducts.com
+            </p>
+          </div>
+        </div>
+        <div className="row mt-5">
+          <div className="col copyright">
+            <p className="">
+              <small className="text-black-50">
+                Copyright © 2021 – ARR Rohini Food Products. All rights
+                reserved. Designed by Call Bharat – B2B & Digital Marketing
+                Services Agency
+              </small>
+            </p>
           </div>
         </div>
       </div>
-      <div className="last">
-        <div className="footer-copyright text-center py-3 ">
-          © 2020 Copyright- – ARR Rohini Food Products. All rights reserved
-          <br></br>
-          Designed by Call Bharat – B2B & Digital Marketing Services Agency
-        </div>
-      </div>
-    </footer>
+    </div>
   );
 }
