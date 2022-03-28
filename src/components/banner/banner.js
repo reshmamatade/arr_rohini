547ef284bedf22bf575fc08f3e3e01dec24cb170
reshmamatade <reshmamatade@gmail.com>
2022-03-28 12:25:41 +0530

    updated code

--- src/components/banner/banner.js
+++ src/components/banner/banner.js
@@ -8,10 +8,10 @@ import Fade from 'react-reveal/Fade';
 
 export default function Banner() {
     return (
-        <div>
+        <div className='banner'>
             <Carousel>
                 <Carousel.Item interval={4000}>
-                    <motion.img transition={{ ease: "easeOut", duration: 2 }} src='./images/bannerchilli.jpg' height="650px" width="100%" alt="Img1" />
+                    <motion.img transition={{ ease: "easeOut", duration: 2 }} src='./images/bannerchilli.jpg' height="auto" width="100%" alt="Img1" />
                     <Carousel.Caption>
                         <div className='txt'> <Fade right><h1>CHILLI <br />POWDER</h1>
                             <h6>ROHINI FOOD Products</h6></Fade></div>
@@ -19,9 +19,9 @@ export default function Banner() {
                     <Fade left><Carousel.Caption><img className="logo" src='./images/ARRLOGO.png' /></Carousel.Caption></Fade>
                 </Carousel.Item>
                 <Carousel.Item interval={4000}>
-                    <motion.img animate={{}} src='./images/bannerhaldi.jpg' height="650px" width="100%" alt="Img2" />
+                    <motion.img animate={{}} src='./images/bannerhaldi.jpg' height="auto" width="100%" alt="Img2" />
                     <Carousel.Item>
-                        <img src='./images/ARRLOGO.png' height="50px" width="5px" alt="Img" />
+                        <img src='./images/ARRLOGO.png' height="auto" width="5px" alt="Img" />
                     </Carousel.Item>
                     <Carousel.Caption>
                         <div className='txt'><Fade right>
@@ -33,7 +33,7 @@ export default function Banner() {
 
                 </Carousel.Item>
                 <Carousel.Item interval={4000}>
-                    <motion.img animate={{}} src='./images/bannerVermicelli.jpg' height="650px" width="100%" alt="Img3" />
+                    <motion.img animate={{}} src='./images/bannerVermicelli.jpg' height="auto" width="100%" alt="Img3" />
                     <Carousel.Caption>
                         <div className='txt'><Fade right>
                             <h1>ROASTED <br /> VERMICELLI</h1>
@@ -44,7 +44,7 @@ export default function Banner() {
 
                 </Carousel.Item>
                 <Carousel.Item interval={4000}>
-                    <motion.img animate={{}} src='./images/bannerWheat.jpg' height="650px" width="100%" alt="Img3" />
+                    <motion.img animate={{}} src='./images/bannerWheat.jpg' height="auto" width="100%" alt="Img3" />
                     <Carousel.Caption>
                         <div className='txt'><Fade right>
                             <h1>CHAKKI<br /> ATTA</h1>
