26d823691d987165fa7701c503b7110aa0db7e47
Himani123 <hmallewar@gmail.com>
2022-03-28 12:05:10 +0530

    about component changes

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -2,16 +2,13 @@ import React from 'react'
 import './about.scss'
 export default function About() {
     return (
-        <div className='container'>
-
+        <div className='container about'>
             <div className='row'>
-
                 <div className='col-md-6'><h2 className='heading'>Rohini Food Products</h2>
-                    <h2 className='second'>From the House of A.R.R</h2>
-
+                    <h2 className='sub-heading'>From the House of A.R.R</h2>
                     <div className='text'>The A.R.R. Group was founded in the year 1947, by late Sri. A.R.Ramaswamy, Ex. Member of Legislative Assembly, Tamil Nadu and Ex.Chairman of Kumbakonam Municipality. Rohini Food Products was founded by Mr. Raman Subramanian and Mrs. Mahesh Raman, Grand Daughter of Sri.A.R.Ramaswamy. It was one of the Intrapreneurship initiatives from the House of A.R.R to extend the product portfolio from Tobbacco products to Food products.
                     </div>
-                    <button className='btn'>CLICK HERE</button>
+                    <button className='btn btn-primary about-button'>CLICK HERE</button>
                 </div>
                 <div className='col-md-6'> <img className='image' src='https://arrrohinifoodproducts.com/wp-content/uploads/2022/01/THMBOOLAM-Product.png' /></div>
             </div>
