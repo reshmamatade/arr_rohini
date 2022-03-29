2a06fab950925c9581138fbc9b8fd594b91c50e0
reshmamatade <reshmamatade@gmail.com>
2022-03-29 11:35:10 +0530

    banner

--- src/components/banner/banner.js
+++ src/components/banner/banner.js
@@ -3,55 +3,53 @@ import React from 'react'
 import { Carousel } from 'react-bootstrap'
 import './banner.scss'
 
-import { motion } from 'framer-motion'
-import Fade from 'react-reveal/Fade';
 
 export default function Banner() {
     return (
         <div className='banner'>
             <Carousel>
                 <Carousel.Item interval={4000}>
-                    <motion.img transition={{ ease: "easeOut", duration: 2 }} src='./images/bannerchilli.jpg' height="auto" width="100%" alt="Img1" />
+                    <img src='./images/bannerchilli.jpg' height="auto" width="100%" alt="Img1" />
                     <Carousel.Caption>
-                        <div className='txt'> <Fade right><h1>CHILLI <br />POWDER</h1>
-                            <h6>ROHINI FOOD Products</h6></Fade></div>
+                        <div className='txt'> <h1>CHILLI <br />POWDER</h1>
+                            <h6>ROHINI FOOD Products</h6></div>
                     </Carousel.Caption>
-                    <Fade left><Carousel.Caption><img className="logo" src='./images/ARRLOGO.png' /></Carousel.Caption></Fade>
+                    <Carousel.Caption><img className="logo" src='./images/ARRLOGO.png' /></Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item interval={4000}>
-                    <motion.img animate={{}} src='./images/bannerhaldi.jpg' height="auto" width="100%" alt="Img2" />
+                    <img src='./images/bannerhaldi.jpg' height="auto" width="100%" alt="Img2" />
                     <Carousel.Item>
                         <img src='./images/ARRLOGO.png' height="auto" width="5px" alt="Img" />
                     </Carousel.Item>
                     <Carousel.Caption>
-                        <div className='txt'><Fade right>
+                        <div className='txt'>
                             <h1>TURMERIC<br /> POWDER</h1>
                             <h6>ROHINI FOOD Products</h6>
-                        </Fade></div>
+                        </div>
                     </Carousel.Caption>
-                    <Fade left><Carousel.Caption><img className="logo" src='./images/ARRLOGO.png' /></Carousel.Caption></Fade>
+                    <Carousel.Caption><img className="logo" src='./images/ARRLOGO.png' /></Carousel.Caption>
 
                 </Carousel.Item>
                 <Carousel.Item interval={4000}>
-                    <motion.img animate={{}} src='./images/bannerVermicelli.jpg' height="auto" width="100%" alt="Img3" />
+                    <img src='./images/bannerVermicelli.jpg' height="auto" width="100%" alt="Img3" />
                     <Carousel.Caption>
-                        <div className='txt'><Fade right>
+                        <div className='txt'>
                             <h1>ROASTED <br /> VERMICELLI</h1>
                             <h6>ROHINI FOOD Products</h6>
-                        </Fade></div>
+                        </div>
                     </Carousel.Caption>
-                    <Fade left><Carousel.Caption><img className="logo" src='./images/ARRLOGO.png' /></Carousel.Caption></Fade>
+                    <Carousel.Caption><img className="logo" src='./images/ARRLOGO.png' /></Carousel.Caption>
 
                 </Carousel.Item>
                 <Carousel.Item interval={4000}>
-                    <motion.img animate={{}} src='./images/bannerWheat.jpg' height="auto" width="100%" alt="Img3" />
+                    <img src='./images/bannerWheat.jpg' height="auto" width="100%" alt="Img3" />
                     <Carousel.Caption>
-                        <div className='txt'><Fade right>
+                        <div className='txt'>
                             <h1>CHAKKI<br /> ATTA</h1>
                             <h6>ROHINI FOOD Products</h6>
-                        </Fade></div>
+                        </div>
                     </Carousel.Caption>
-                    <Fade left><Carousel.Caption><img className="logo" src='./images/ARRLOGO.png' /></Carousel.Caption></Fade>
+                    <Carousel.Caption><img className="logo" src='./images/ARRLOGO.png' /></Carousel.Caption>
 
                 </Carousel.Item>
             </Carousel>
